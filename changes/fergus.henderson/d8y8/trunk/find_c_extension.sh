6b946902a9e73677827420cd787ef7aacfc03dce
fergus.henderson <fergus.henderson@01de4be4-8c4a-0410-9132-4925637da917>
2008-05-08 20:12:45 +0000

    Update web pages to correct statements that are no longer accurate for distcc 3.0.

---
+++ changes/fergus.henderson/d8y8/trunk/find_c_extension.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+# Usage: find_c_extension.sh BUILDDIR
+#
+# Write path of directory containing C-extension of build directory.
+#
+# More precisely, locate the unique directory of the form:
+#
+#   _builddir/lib.*/include_server/
+#
+# that contains file 'distcc_pump_c_extensions.so'. Write the path of this 
+# directory to stdout and exit with status 0. If such a path does not exist 
+# then write error message to stderr and exit with status 1.
+
+builddir=$1
+so_files=`ls $builddir/_builddir/lib.*/include_server/\
+distcc_pump_c_extensions.so`
+if [ -z "$so_files" ]; then
+  echo \
+    '__________Could not find shared libraries for distcc-pump' 1>&2
+  exit 1
+elif echo $so_files | grep -q ' '; then
+  echo \
+    '__________Shared libraries for multiple architectures discovered.' \
+    1>&2
+  echo \
+    "__________Cannot determine which one to use among: $so_files" \
+    1>&2
+  exit 1
+else
+  # There was only one such file.
+  dirname $so_files
+  exit 0
+fi
---
+++ changes/fergus.henderson/d8y8/trunk/src/clinet.h
@@ -0,0 +1,33 @@
+/* -*- c-file-style: "java"; indent-tabs-mode: nil -*-
+ * 
+ * distcc -- A simple distributed compiler system
+ *
+ * Copyright (C) 2002, 2003 by Martin Pool <mbp@samba.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ * USA
+ */
+
+struct sockaddr;
+
+int dcc_connect_by_name(const char *host,
+                        int port,
+                        int *p_fd);
+                        
+int dcc_connect_by_addr(struct sockaddr *sa, 
+                        size_t salen,
+                        int *p_fd);
+                        
+
