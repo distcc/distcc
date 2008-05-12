cc9aa5984b30b15865514a1dca689425af67de51
fergus.henderson <fergus.henderson@01de4be4-8c4a-0410-9132-4925637da917>
2008-05-12 19:11:22 +0000

    Add support for pump mode to the benchmarking scripts.
    Also add support for setting the number of hosts to use.
    
    Fix division by zero error that occurred when running benchmark script with the "-a" option.
    
    Fix broken URL for GNU hello package.
    
    Reviewers: Craig Silverstein, Nils Klarlund

--- trunk/bench/compiler.py
+++ trunk/bench/compiler.py
@@ -18,46 +18,92 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
 
+import buildutil
+import os
+
 class CompilerSpec:
     """Describes a compiler/make setup.
 
     Used to define different situations such as local compilation, and
     various degrees of parallelism."""
-    
-    def __init__(self, cc=None, cxx=None, make_opts=None, name=None):
-        self.cc = cc or 'gcc'
-        self.cxx = cxx or 'c++'
-        self.make_opts = make_opts or ''
-        self.name = name or (self.cc + "__" + self.make_opts).replace(' ', '_')
+
+    def __init__(self, cc='cc', cxx='c++', make_opts='',
+                 pump_cmd='', num_hosts=1, host_opts='',
+                 name=None):
+        self.cc = cc
+        self.cxx = cxx
+        self.make_opts = make_opts
+        self.host_opts = host_opts
+        self.pump_cmd = pump_cmd
+        self.num_hosts = num_hosts
+        self.host_opts = host_opts
+        self.name = name or (self.pump_cmd + self.cc + "__" +
+                             self.make_opts).replace(' ', '_')
 
 
 def default_compilers():
-    return [parse_opt('local,1'),
-            parse_opt('dist,8'),
+    return [parse_opt('local,h1,j1'),
+            parse_opt('dist,h10,j20'),
+            parse_opt('dist,h10,j40'),
+            parse_opt('pump,h10,j20'),
+            parse_opt('pump,h10,j40'),
             ]
 
 def parse_opt(optarg):
     """Parse command-line specification of a compiler
 
     XXX: I don't really know what the best syntax for this is.  For
-    the moment, it is "local" or "dist", followed by a comma and a
-    -j number.  Perhaps we need to be able to specify host lists here
-    too.
+    the moment, it is "local", "dist", or "pump", followed by ",h"
+    and the number of hosts to use, followed by ",j" and the number
+    of jobs to use (for the -j option to make).
     """
-    where, howmany = optarg.split(',')
-    howmany = int(howmany)
+    where, hosts, jobs = optarg.split(',')
+    if hosts.startswith("h"):
+      hosts = int(hosts[1:])
+      if not os.getenv("DISTCC_HOSTS"):
+        raise ValueError, "You must set DISTCC_HOSTS before running benchmarks"
+      max_hosts = buildutil.count_hosts(os.getenv("DISTCC_HOSTS"))
+      if hosts > max_hosts:
+        print ("Warning: can't use %d hosts: DISTCC_HOSTS only has %d" %
+               (hosts, max_hosts))
+        hosts = max_hosts
+    else:
+      raise ValueError, ("invalid compiler option: "
+                         "expecting '...,h<NUMBER OF HOSTS>,...', found %s"
+                         % `hosts`)
+    if jobs.startswith("j"):
+      jobs = int(jobs[1:])
+    else:
+      raise ValueError, ("invalid compiler option: "
+                         "expecting '...,j<NUMBER OF JOBS>', found %s"
+                         % `hosts`)
     if where == 'local':
-        return CompilerSpec(name='local_%02d' % howmany,
+        return CompilerSpec(name='local_%02d' % jobs,
                             cc='cc',
                             cxx='c++',
-                            make_opts='-j%d' % howmany)
+                            num_hosts=1,
+                            make_opts='-j%d' % jobs)
     elif where == 'dist':
-        return CompilerSpec(name='dist_%02d' % howmany,
+        return CompilerSpec(name='dist_h%02d_j%02d' % (hosts, jobs),
                             cxx='distcc c++',
                             cc='distcc cc',
-                            make_opts='-j%d' % howmany)
+                            num_hosts=hosts,
+                            make_opts='-j%d' % jobs)
+    elif where == 'lzo':
+        return CompilerSpec(name='lzo_h%02d_j%02d' % (hosts, jobs),
+                            cxx='distcc c++',
+                            cc='distcc cc',
+                            num_hosts=hosts,
+                            host_opts=",lzo",
+                            make_opts='-j%d' % jobs)
+    elif where == 'pump':
+        return CompilerSpec(name='pump_h%02d_j%02d' % (hosts, jobs),
+                            cxx='distcc c++',
+                            cc='distcc cc',
+                            pump_cmd='pump ',
+                            num_hosts=hosts,
+                            host_opts=",cpp,lzo",
+                            make_opts='-j%d' % jobs)
     else:
-        raise ValueError, ("don't understand %s" % `where`)
-
-
-    
+      raise ValueError, ("invalid compiler option: don't understand %s"
+                         % `where`)
