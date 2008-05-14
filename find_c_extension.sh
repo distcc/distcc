785cae08734a7b699f2e894be2b2b182c8d53e51
Craig Silverstein <csilvers@khanacademy.org>
2008-05-14 00:31:35 +0000

    A few more cleanups:
    1) No ! outside of a 'test' (sh-compatibility).
    2) grep >/dev/null instead of grep -q (solaris portability).
    3) Improved the quote hygenie a little.
    4) prepend to PYTHONPATH instead of overriding it.
    
    Tested by running on a solaris machine, and on a machine where
    PYTHONPATH is needed to locate the python distribution (which is not
    in the standard location).
    
    Reviewed by fergus@google.com

--- find_c_extension.sh
+++ find_c_extension.sh
@@ -12,14 +12,14 @@
 # directory to stdout and exit with status 0. If such a path does not exist 
 # then write error message to stderr and exit with status 1.
 
-builddir=$1
+builddir="$1"
 so_files=`ls $builddir/_include_server/lib.*/include_server/\
 distcc_pump_c_extensions.so`
 if [ -z "$so_files" ]; then
   echo \
     '__________Could not find shared libraries for distcc-pump' 1>&2
   exit 1
-elif echo $so_files | grep -q ' '; then
+elif echo "$so_files" | grep ' ' >/dev/null; then
   echo \
     '__________Shared libraries for multiple architectures discovered.' \
     1>&2
@@ -29,6 +29,6 @@ elif echo $so_files | grep -q ' '; then
   exit 1
 else
   # There was only one such file.
-  dirname $so_files
+  dirname "$so_files"
   exit 0
 fi
