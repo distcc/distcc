e219e85b33e5b4512d6a546c9e4f53f941af9ae6
csilvers <csilvers@01de4be4-8c4a-0410-9132-4925637da917>
2008-05-15 01:15:32 +0000

    The tmpdir-creation routine has an (undocumented) invariant that the tmpdir should not end in a slash -- at least, inside distcc, you see code like this:    snprintf("%s/%s", tmpdir, filename)
    On the other hand, windows's routine to create a tempdir guarantees it
    will end with a slash.  I'm not sure it really matters, but get rid of
    the slash in that case.  That way we don't have to worry about how
    filenames are interpreted (since // does have a special meaning for
    filenames in windows, though probably not in a way this code could
    generate before; still, no harm in normalizing to one slash).
    
    Tested by running 'make check' on cygwin.
    
    Reviewed by fergus@google.com

--- trunk/src/tempfile.c
+++ trunk/src/tempfile.c
@@ -178,7 +178,7 @@ int dcc_get_new_tmpdir(char **tempdir)
     return 0;
 }
 
-
+/* This function returns a directory-name, it does not end in a slash. */
 int dcc_get_tmp_top(const char **p_ret)
 {
 #ifdef __CYGWIN32__
@@ -188,8 +188,11 @@ int dcc_get_tmp_top(const char **p_ret)
     int f,ln;
     GetTempPath(MAXPATHLEN+1,s);
     /* Convert slashes */
-    for(f=0,ln=strlen(s);f!=ln;f++)
+    for (f = 0, ln = strlen(s); f != ln; f++)
 	    if (s[f]=='\\') s[f]='/';
+    /* Delete trailing slashes -- but leave one slash if s is all slashes */
+    for (f = strlen(s)-1; f > 0 && s[f] == '/'; f--)
+	    s[f]='\0';
 
     if ((ret = dcc_add_cleanup(s))) {
 	    free(s);
