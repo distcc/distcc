b7d1aa7b2e8c1c3f5091e68a79e1040c8c1e73f0
klarlund <klarlund@01de4be4-8c4a-0410-9132-4925637da917>
2008-05-16 01:06:29 +0000

    Fix: speed-up the include processor a few times by exporting PYTHONOPTIMIZE.
    This took a little while: fortunately, the tracing information is dependent on
    the PYTHONOPTIMIZE setting, so after a diff on the outputs of the gws example
    before and after, it finally dawned upon me what was wrong.
    
    Tested: manually on the gws example with 600 include directories.

--- changes/klarlund/b5c9/trunk/find_c_extension.sh
+++ changes/klarlund/b5c9/trunk/find_c_extension.sh
@@ -13,8 +13,13 @@
 # then write error message to stderr and exit with status 1.
 
 builddir="$1"
+# We want to catch the file whether it's .so (unix) or .dll (cygwin).
+# The '[sd][ol]*' glob does that without having to worry about
+# unpredictable 'ls' behavior when a glob has no matches.
+# (Side note: there is intense internal debate about whether the
+# '[sd][ol]' hack is "so ugly I can't stand it" or "kinda cute".)
 so_files=`ls $builddir/_include_server/lib.*/include_server/\
-distcc_pump_c_extensions.so`
+distcc_pump_c_extensions.[sd][ol]*`
 if [ -z "$so_files" ]; then
   echo \
     '__________Could not find shared libraries for distcc-pump' 1>&2
