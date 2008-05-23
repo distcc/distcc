1775165a0365bfb9ce28714cb47e92cba075c260
klarlund <klarlund@01de4be4-8c4a-0410-9132-4925637da917>
2008-05-23 04:17:07 +0000

    Make the Linux 2.6 kernel build with distcc-pump mode.
    More specifically, add flag --send_systemdirs.  For the kernl, this will allow
    the compiler headers that pertain to a few -isystem occurrences to be shipped to
    the servers.  Without this flag setting, distcc quickly decides that pump
    mode is not viable because a remote compilation fails.
    
    Tests: make pump-maintainer-check
           make include-server-maintainer-check
           make 'linux kernel' in a directory made by the benchmark script (as shown below)
    
    DISTCC_EMAILLOG_WHOM_TO_BLAME=klarlund@google.com DISTCC_ENABLE_DISCREPANCY_EMAIL=1 INCLUDE_SERVER_ARGS='-d1 -t --email --unsafe_absolute_includes --send_systemdirs --stat_reset_triggers=include/linux/*' DISTCC_HOSTS="--randomize `/home/klarlund/svn-distcc/distcc/lsdistcc -P3 -k100 -d`" PATH=/home/klarlund/svn-distcc/distcc:$PATH  ./masquerade pump make -j120
    
    Triva: the about 1000 files in the kernel build involve on average 400
    headers. The build time on a quad-cpu machine drops from about 1m40 s to
    1m20s. With pump-mode, the machine is underutilized judging from the total sys +
    user time, which is around only 2m20s.

---
+++ changes/klarlund/g6n8/trunk/include_server/mirror_path_test.py
@@ -0,0 +1,145 @@
+#! /usr/bin/python2.4
+
+# Copyright 2007 Google Inc.
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ 
+__author__ = "Nils Klarlund"
+
+import os
+import os.path
+
+import basics
+import cache_basics
+import mirror_path
+import unittest
+
+NotCoveredError = basics.NotCoveredError
+
+
+class MirrorPathTest(unittest.TestCase):
+
+  """We construct a mock-up world of a file system in order to
+  unittest the DoPath function of mirror_path."""
+
+
+  def setUp(self):
+
+    basics.debug_pattern = 3
+
+    caches = cache_basics.SetUpCaches()
+
+    self.canonical_path = caches.canonical_path
+    self.simple_build_stat = caches.simple_build_stat
+    self.mirror_path = mirror_path.MirrorPath(self.simple_build_stat,
+                                              self.canonical_path)
+
+    self.directories = ['/', '/a', '/link', '/a/link', '/a/b',
+                        '/link/link', '/root']
+    self.links = ['/a/link', '/link', '/link/link']
+    self.exists = self.directories + self.links
+    self.realpaths = {'/'         :'/',
+                      '/a'        :'/a',
+                      '/a/link'   :'/a/b',
+                      '/link'     :'/a',
+                      '/link/link':'/a/b'}
+
+
+  def tearDown(self):
+    pass
+
+  def test_MirrorPath(self):
+
+    try:
+
+      def isdir(path):
+        return path in self.directories
+      def exists(path):
+        return path in self.exists
+      def islink(path):
+        return path in self.links
+      def realpath(path):
+        if path.startswith('/root'):
+          self.fail("Not expected that '%s' startd with '/root'." % path)
+        return self.realpaths[path]
+      def makedirs(path):
+        if path == '/root/a':
+          self.directories.extend(['/root/a'])
+          self.exists.extend(['/root/a'])
+        else:
+          self.fail("makedirs %s" % path)
+      def symlink(src, dest):
+        if not (src, dest) in [ ('/a', '/root/link'),
+                                ('/a/b', '/root/a/link') ]:
+          self.fail("symlink %s %s" % (src, dest))
+        self.links.append(dest)
+        self.exists.append(dest)
+      # Overwrite the canonicalization function that MirrorPath uses.
+      self.mirror_path.canonical_path.Canonicalize = realpath
+      # Overwrite various system functions that MirrorPath uses.
+      isdir_ = os.path.isdir
+      os.path.isdir = isdir
+      exists_ = os.path.exists
+      os.path.exists = exists
+      islink_ = os.path.islink
+      os.path.islink = islink
+      makedirs_ = os.makedirs
+      os.makedirs = makedirs
+      symlink_ = os.symlink
+      os.symlink = symlink
+
+      # Mirror the link /a/link.
+      self.mirror_path.DoPath('/a/link', 117, '/root')
+      self.assertEqual(self.mirror_path.Links(),  ['/root/a/link'])
+      self.assert_(self.simple_build_stat.Lookup('/root/a'))
+
+      # Check that symlink function is not called again, by verifying
+      # that mirror_path.Links() doesn't grow.
+      self.mirror_path.DoPath('/a/link', 117, '/root')
+      self.assertEqual(self.mirror_path.Links(), ['/root/a/link'])
+
+      # Now mirror /link/link.
+      self.mirror_path.DoPath('/link/link',  117, '/root')
+      self.assertEqual(self.mirror_path.Links(), ['/root/a/link', '/root/link'])
+      self.assertEqual(
+        [ d for d in self.directories if d.startswith('/root') ],
+        [ '/root', '/root/a' ])
+      self.assertEqual(
+        [ d for d in self.links if d.startswith('/root') ],
+        [ '/root/a/link', '/root/link' ])
+
+      # Now mirror /a/b. Since b is a file and /a already is mirrored,
+      # there is no effect.
+      self.mirror_path.DoPath('/a/b',  117, '/root')
+      self.assertEqual(self.mirror_path.Links(), ['/root/a/link', '/root/link'])
+      self.assertEqual(
+        [ d for d in self.directories if d.startswith('/root') ],
+        [ '/root', '/root/a' ])
+      self.assertEqual(
+        [ d for d in self.links if d.startswith('/root') ],
+        [ '/root/a/link', '/root/link' ])
+
+    finally:
+      try:
+        # Don't propagate another exception.
+        os.path.isdir = isdir_
+        os.path.exists = exists_
+        os.path.islink = islink_
+        os.makedirs = makedirs_
+        os.symlink = symlink_
+      except NameError:
+        pass
+
+unittest.main()
