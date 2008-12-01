ad7bf987a01a34534cefe63f3c2748d627385427
fergus.henderson <fergus.henderson@01de4be4-8c4a-0410-9132-4925637da917>
2008-12-01 23:10:41 +0000

    Bump version number to 3.1, and update the NEWS file and ChangeLog.

---
+++ changes/fergus.henderson/i8s3/trunk/src/zeroconf.h
@@ -0,0 +1,36 @@
+/* -*- c-file-style: "java"; indent-tabs-mode: nil; tab-width: 4; fill-column: 78 -*-
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ */
+
+#ifndef foozeroconfhfoo
+#define foozeroconfhfoo
+
+#include <inttypes.h>
+
+int dcc_zeroconf_add_hosts(struct dcc_hostdef **re_list, int *ret_nhosts, int slots, struct dcc_hostdef **ret_prev);
+
+void *dcc_zeroconf_register(uint16_t port, int n_cpus);
+int dcc_zeroconf_unregister(void*);
+
+char* dcc_get_gcc_version(char *s, size_t nbytes);
+char* dcc_get_gcc_machine(char *s, size_t nbytes);
+
+char* dcc_make_dnssd_subtype(char *stype, size_t nbytes, const char *v, const char *m);
+
+#define DCC_DNS_SERVICE_TYPE "_distcc._tcp"
+
+#endif
