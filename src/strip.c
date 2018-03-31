a122536e8793cc8484997125d7da7ea740b84f63
Michał Górny <mgorny@gentoo.org>
2018-03-31 09:35:46 +0200

    Strip compiler arguments starting with '-isystem'
    The chromium build system (using clang) apparently concatenates
    '-isystem' with path (i.e. uses it without space). Support stripping
    this version as well to avoid the lot of warnings:
    
        x86_64-pc-linux-gnu-clang++-6.0: warning: argument unused during compilation: '-isystem /usr/include/nss' [-Wunused-command-line-argument]

--- src/strip.c
+++ src/strip.c
@@ -107,7 +107,8 @@ int dcc_strip_local_args(char **from, char ***out_argv)
                  || str_startswith("-L", from[from_i])
                  || str_startswith("-MF", from[from_i])
                  || str_startswith("-MT", from[from_i])
-                 || str_startswith("-MQ", from[from_i])) {
+                 || str_startswith("-MQ", from[from_i])
+                 || str_startswith("-isystem", from[from_i])) {
             /* Something like "-DNDEBUG" or
              * "-Wp,-MD,.deps/nsinstall.pp".  Just skip this word */
             ;
