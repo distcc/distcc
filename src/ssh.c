3ca2de25d630bfaf7fd4bd594a57902c61db1c14
Shawn Landden <shawn@git.icu>
2018-07-25 20:33:25 -0700

    ssh: fix segfault
    when DISTCC_SSH is not set
    
    fixes da14374f

--- src/ssh.c
+++ src/ssh.c
@@ -198,12 +198,12 @@ int dcc_ssh_connect(char *ssh_cmd,
     char *child_argv[10+max_ssh_args];
     int i,j;
     int num_ssh_args = 0;
+    char *ssh_cmd_in;
 
     /* We need to cast away constness.  I promise the strings in the argv[]
      * will not be modified. */
 
-    if (!ssh_cmd) {
-        char *ssh_cmd_in = getenv("DISTCC_SSH");
+    if (!ssh_cmd && (ssh_cmd_in = getenv("DISTCC_SSH"))) {
         ssh_cmd = strtok(ssh_cmd_in, " ");
         char *token = strtok(NULL, " ");
         while (token != NULL) {
