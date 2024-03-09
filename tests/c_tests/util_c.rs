6ff5c74ac6f4e0d923cb254bd45c25b9a1454b2d
Martin Pool <mbp@sourcefrog.net>
2024-03-09 18:41:00 +0000

    dcc_which shouldn't segv when compiler is not found
    Based on <https://github.com/distcc/distcc/pull/500>
    
    Also add a Rust test for this

--- tests/c_tests/util_c.rs
+++ tests/c_tests/util_c.rs
@@ -20,4 +20,23 @@ fn dcc_which_finds_system_cc() {
     let path = PathBuf::from(&path_cstr.to_str().unwrap());
     assert!(path.exists());
     assert!(path.ends_with("cc"));
+    unsafe { libc::free(found_path as *mut std::ffi::c_void) };
+}
+
+/// Test the case described in <https://github.com/distcc/distcc/pull/500> and
+/// #497: if the compiler is not found, distcc will segfault, but
+/// actually it should return an error.
+///
+/// Before that was fixed, this test reproduced the segfault.
+#[test]
+fn dcc_which_nonexistent_command_not_found() {
+    let mut found_path: *mut c_char = null_mut();
+    let ret = unsafe {
+        c::dcc_which(
+            c"cc_____NONEXISTENT_____1234".as_ptr(),
+            &mut found_path as *mut *mut c_char,
+        )
+    };
+    assert_eq!(ret, -libc::ENOENT);
+    assert!(found_path.is_null());
 }
