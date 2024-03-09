fcf8cc8722964de69fbf675ae82a54a11350ae56
Martin Pool <mbp@sourcefrog.net>
2024-03-09 23:19:20 +0000

    Fix up mapping argv to c; rename binary

--- src/bin/distcc.rs
+++ src/bin/distcc.rs
@@ -3,29 +3,24 @@
 use std::ffi::{c_char, c_int, CString};
 use std::iter::once;
 use std::process::ExitCode;
-use std::ptr::null;
+use std::ptr::null_mut;
 
 use distcc::c;
 
 fn main() -> ExitCode {
-    println!("Hello from distcc-rs!");
-
-    // Map argv to C.
     let args = std::env::args()
         .map(|arg| CString::new(arg).unwrap())
         .collect::<Vec<CString>>(); // kept for lifetime
+    let argc = args.len();
     let mut c_args = args
-        .iter()
-        .map(|arg| arg.as_ptr())
-        .chain(once(null()))
-        .collect::<Vec<*const c_char>>();
+        .into_iter()
+        .map(|arg| arg.into_raw())
+        .chain(once(null_mut()))
+        .collect::<Vec<*mut c_char>>();
     let err: u8 = unsafe {
-        c::distcc_main(
-            c_args.len() as c_int,
-            c_args.as_mut_ptr() as *mut *mut c_char,
-        )
-        .try_into()
-        .expect("Exit code from C fits in u8")
+        c::distcc_main(argc as c_int, c_args.as_mut_ptr())
+            .try_into()
+            .expect("Exit code from C fits in u8")
     };
     dbg!(err);
     ExitCode::from(err)
